fa85ce803e9b1ee8ca4158471ee2b2ea40cdc63a
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2017-04-11 15:22:18 +0200

    Added the project

---
+++ QR Project/Assets/Our C# Scripts/RunOnStartUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RunOnStartUp : MonoBehaviour {
+
+    [InitializeOnLoad]
+    public class Startup
+    {
+        static Startup()
+        {
+            //Application.LoadLevel("FirstScene");
+            SceneManager.LoadScene("FirstScene");
+        }
+    }
+}
