4c3afb8f3d251dc62c212af7e0b3e0831a0b62fe
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2017-04-13 13:03:00 +0200

    added the android hardware back button

---
+++ QR Project/Assets/Our C# Scripts/AndroidBackButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidBackButton : MonoBehaviour {
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            GoToOtherScene SceneNavigator = new GoToOtherScene();
+            SceneNavigator.GoBackButton();
+        }
+    }
+
+
+}
